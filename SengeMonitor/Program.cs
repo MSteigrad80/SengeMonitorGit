b8b44ef15c0aa5563babdb33a4fb886597d2a695
MSteigrad80 <55984659+MSteigrad80@users.noreply.github.com>
2022-03-01 16:08:24 +0100

    Erfaasung 1

--- SengeMonitor/Program.cs
+++ SengeMonitor/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace SengeMonitor
+{
+    public class Program
+    {
+        string DSN = string.Format(@"C:\SengDataNeu\");
+        string DSNNeu = string.Format(@"C:\Users\ManfredSteigrad\OneDrive - M.Steigrad\SenDataAquire\");
+
+        private int TimeOutMillis = 2000;
+        FileSystemWatcher fsw = new FileSystemWatcher(@"C:\SengDataNeu\");
+        System.Threading.Timer m_timer = null;
+        List<string> files  = new List<string>();
+
+
+    }
+}
