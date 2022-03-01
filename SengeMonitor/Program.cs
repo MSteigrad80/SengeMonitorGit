b67f43e780af7d00a75e9dadbe74088fd43759f9
MSteigrad80 <55984659+MSteigrad80@users.noreply.github.com>
2022-03-01 17:32:46 +0100

    Vollständig

--- SengeMonitor/Program.cs
+++ SengeMonitor/Program.cs
@@ -15,6 +15,53 @@ namespace SengeMonitor
         System.Threading.Timer m_timer = null;
         List<string> files  = new List<string>();
 
+        static void main(string[] args)
+        {
+            Program program = new Program();
+            program.Initial();
+        }
+        public void Initial()
+        {
+            fsw.NotifyFilter = NotifyFilters.LastWrite;
+            fsw.Filter = "*.csv";
+            fsw.Changed += new FileSystemEventHandler(OnChanged);
+            fsw.Created += new FileSystemEventHandler(OnChanged);
+            fsw.Deleted += new FileSystemEventHandler(OnChanged);
 
+            fsw.EnableRaisingEvents = true;
+            if (m_timer == null)
+            {
+                m_timer = new System.Threading.Timer(new
+                    System.Threading.TimerCallback(OnWatchedFileChange), null,
+                    Timeout.Infinite, Timeout.Infinite);
+            }
+            Console.WriteLine("Press \'Enter\' to quit");
+            Console.ReadLine();
+        }
+        void OnChanged(Object s, FileSystemEventArgs e)
+        {
+            Mutex mutex = new Mutex(false, "FSW");
+            mutex.WaitOne();
+            if (!files.Contains(e.Name))
+            {
+                files.Add(e.Name);
+            }
+            mutex.ReleaseMutex();
+            m_timer.Change(TimeOutMillis, Timeout.Infinite);
+        }
+        public void OnWatchedFileChange(object state)
+        {
+            List<string> backup = new List<string>();
+            Mutex mutex = new Mutex(false, "FSW");
+            mutex.WaitOne();
+            backup.AddRange(files);
+            files.Clear();
+            mutex.ReleaseMutex();
+            foreach(string file in backup)
+            {
+                Console.WriteLine(DSN + file + " changed");
+                File.Copy(DSN + file, DSNNeu + file, true);
+            }
+        }
     }
 }
